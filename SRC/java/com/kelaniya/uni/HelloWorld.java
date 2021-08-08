58c78988306c3cbba65fe6f8b5d4035813743b1c
Poornajith <chiranthapoornajith@gmail.com>
2021-08-08 17:59:24 +0530

    second commit

--- SRC/java/com/kelaniya/uni/HelloWorld.java
+++ SRC/java/com/kelaniya/uni/HelloWorld.java
@@ -1,2 +1,9 @@
-package com.kelaniya.uni;public class HelloWorld {
+package com.kelaniya.uni;
+int i=0;
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+
 }
