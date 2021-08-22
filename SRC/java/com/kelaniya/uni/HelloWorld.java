cfe5224619388867e528caf2a90e579f8773af75
Poornajith <chiranthapoornajith@gmail.com>
2021-08-22 20:04:43 +0530

    v2. Input validation

--- SRC/java/com/kelaniya/uni/HelloWorld.java
+++ SRC/java/com/kelaniya/uni/HelloWorld.java
@@ -1,9 +1,21 @@
 package com.kelaniya.uni;
-int i=0;
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("hello world");
+        // make sure that the arguments are provided
+        if (args.length ==0) {
+            System.out.println("please provide operation as an argument");
+            return;
+        }
+        String operator = args[0];
+
+        // add sub mul
+        // validation for if user input wrong arguments
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul") )) {
+            System.out.println("please provide add, sub or mul ad operator argument");
+            return;
+        }
+                System.out.println(args[0]);
     }
 
 }
