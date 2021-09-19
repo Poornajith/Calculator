872a12ca05cbbd19e0451a53648c4573f5435773
Poornajith <chiranthapoornajith@gmail.com>
2021-09-19 22:00:16 +0530

    unit testing

--- SRC/java/com/kelaniya/uni/v5/operations/DivOperation.java
+++ SRC/java/com/kelaniya/uni/v5/operations/DivOperation.java
@@ -1,7 +1,11 @@
 package com.kelaniya.uni.v5.operations;
 
 public class DivOperation {
-    public Double execute(Double[] numbers) {
+    public Double execute(Double[] numbers) throws InvalidOperationExecption {
+
+        if (numbers[1] == 0) {
+            throw new InvalidOperationExecption("Do not divide by zero");
+        }
 
         return numbers[0] / numbers[1];
 
---
+++ SRC/java/com/kelaniya/uni/v5/operations/InvalidOperationExecption.java
@@ -0,0 +1,7 @@
+package com.kelaniya.uni.v5.operations;
+
+public class InvalidOperationExecption extends Throwable {
+    public InvalidOperationExecption(String do_not_divide_by_zero) {
+        super(message);
+    }
+}
